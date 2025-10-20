beb99988fded1b576c2fc06c22b5b96c15c38001
kimjaeheung <kimjaeheung@gmail.com>
2025-10-21 00:01:42 +0900

    fix: calendar_screen에 kDebugMode import 추가

--- ai_diary_app/lib/screens/calendar_screen.dart
+++ ai_diary_app/lib/screens/calendar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:go_router/go_router.dart';
